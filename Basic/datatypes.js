4b93fc74020113d26f7a30bf2689101a3edcb5c2
Yuvraj_Ithape <yuvrajithape9696@gmail.com>
2024-01-21 10:35:53 +0530

    Understanding Data Types of Js

---
+++ Basic/datatypes.js
@@ -0,0 +1,10 @@
+// Two main data types in javascript 1. Primitive 2. Non-Primitive 
+// All primitive data follows call by value . there are 7 types of primitive data type 
+// Primitive data type : predefined by language and named by reserved keyword 
+// string , Boolean , Number , Bigint , NULL , symbol(unique) , Undefined 
+// Non-Primitive data type - > Array , Object , Function 
+// Array is collection of different types of data items stored in squre bracket 
+
+let arr = ["yuvraj " , 46 ];
+console.log(typeof arr)
+console.og("happy")
