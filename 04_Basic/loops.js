ae161eabe31aad2d0e2f91b0994d6e79bc6368ce
Yuvraj_Ithape <yuvrajithape9696@gmail.com>
2024-01-29 20:04:57 +0530

    For , While , Do While Loops

---
+++ 04_Basic/loops.js
@@ -0,0 +1,23 @@
+// Why we use Loops :
+// When you want to repeat a set of statement an infinite number of times until condition is satisfied 
+// Palindrome : The word , phrase or sequence that reads the backward as forward 
+
+// break statement is used to jump out of loop 
+// continue statement break one iteration in loop if specified condition occur and continues with next iteration in loop 
+
+// for(let i = 1 ; i <= 10 ; i++)
+// {   
+    
+//     console.log((5*i));
+// }
+let i = 1 ;
+// while ( i <=10) {
+//     console.log(i*5);
+//     i++
+// }
+
+do{
+    console.log(i*5);
+    i++;
+
+}while(i <= 10)
