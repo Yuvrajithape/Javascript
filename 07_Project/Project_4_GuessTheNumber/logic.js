08b686f01d0a37235a40c8dcc498e5badb2f98f7
Yuvraj_Ithape <yuvrajithape9696@gmail.com>
2024-03-13 18:45:20 +0530

    Guess the number game

---
+++ 07_Project/Project_4_GuessTheNumber/logic.js
@@ -0,0 +1,105 @@
+let randomNumber = parseInt(Math.random()*100+1);
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrhi = document.querySelector('.lowOrHi');
+const startover = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1 ;
+
+let playGame = true;
+if(playGame)
+{
+    submit.addEventListener('click' , function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
+}
+
+function validateGuess(guess){
+       if(isNaN(guess))
+       {
+        alert("Plese Enter a valid number ");
+       }
+       else if( guess < 1)
+       {
+        alert("Plese Enter a  number Less than 100");
+       }
+       else if( guess > 100)
+       {
+        alert("Plese Enter a number greater than 1 ");
+       }
+       else{
+        prevGuess.push(guess);
+        if(numGuess === 11)
+        {
+            displayGuess(guess);
+            displayMessage(`Game Over. Random number was ${randomNumber}`)
+            endGame();
+        }
+        else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+
+       }
+}
+function checkGuess(guess){
+    if(guess === randomNumber)
+    {
+        displayMessage(`You guessed it right ! `)
+    }
+    else if(guess < randomNumber)
+    {
+        displayMessage(`Number is too low`)
+    }
+    else if(guess > randomNumber)
+    {
+        displayMessage(`Number is too high`)
+    }
+
+}
+function displayGuess(guess)
+{
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}  `;
+    numGuess++;
+    remaining.innerHTML = `${11-numGuess}`;
+
+
+}
+function displayMessage(message){
+      lowOrhi.innerHTML = `<h2> ${message} </h2>`
+}
+function endGame(){
+    userInput.value= '';
+    userInput.setAttribute('disabled' , '');
+    p.classList.add('button');
+   p.innerHTML = `<h2 id="newGame">Start new Game </h2>`
+   startover.appendChild(p);
+   playGame = false;
+   newGame();
+    
+
+}
+function newGame(){
+    const newGamebut = document.querySelector("#newGame");
+    newGamebut.addEventListener('click' , function(e){
+        randomNumber = parseInt(Math.random()*100+1);
+        prevGuess = [];
+        numGuess = 1 ;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11-numGuess}`;
+        userInput.removeAttribute('disabled');
+        startover.removeChild(p);
+        playGame = true;
+
+    })
+
+}
